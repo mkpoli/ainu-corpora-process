fa52be1cd7c706f91a276becd9eb3f35dfa5c40a
mkpoli <git@mkpo.li>
2024-12-12 11:00:52 +0900

    feat: add primitive tokenizer

---
+++ utils/test_tokenize.py
@@ -0,0 +1,46 @@
+from utils.tokenize import tokenize
+
+
+def test_tokenize():
+    TEST_CASES = [
+        [
+            "aynu itak k=eyaypakasnu kor k=an!",
+            ["aynu", "itak", "k=", "eyaypakasnu", "kor", "k=", "an"],
+        ],
+        [
+            "‘sine ancikar i=rewsire wa i=kore yan.’sekor hawean w_a a=rewsire kor oraun ene hawean h_i. an=an. an=uske. ruwe'",
+            [
+                "sine",
+                "ancikar",
+                "i=",
+                "rewsire",
+                "wa",
+                "i=",
+                "kore",
+                "yan",
+                "sekor",
+                "hawean",
+                "wa",
+                "a=",
+                "rewsire",
+                "kor",
+                "oraun",
+                "ene",
+                "hawean",
+                "hi",
+                "an=",
+                "an",
+                "an=",
+                "uske",
+                "ruwe",
+            ],
+        ],
+    ]
+    for text, expected in TEST_CASES:
+        tokens = tokenize(text)
+        print(tokens)
+        assert tokens == expected
+
+
+if __name__ == "__main__":
+    test_tokenize()
---
+++ utils/tokenize.py
@@ -0,0 +1,71 @@
+import re
+from nltk.tokenize import RegexpTokenizer
+
+
+def tokenize(sentence: str) -> list[str]:
+    # 小文字化
+    sentence = sentence.lower()
+    # 下線符号の除去
+    sentence = sentence.replace("_", "")
+    # 単引用符の除去
+    sentence = re.sub(r"[‘'](.*?)['’]", r"\1", sentence)
+    sentence = re.sub(r"(.*?)['’]", r"\1", sentence)
+    sentence = re.sub(r"[‘'](.*?)", r"\1", sentence)
+    sentence = re.sub(r"\[\d+\]", "", sentence)
+
+    tokenizer = RegexpTokenizer(r"[a-zA-Z0-9=-_'’]+")
+    words = tokenizer.tokenize(sentence)
+
+    def split_affixes(word: str) -> list[str]:
+        if word.count("=") + word.count("-") >= 2:
+            PREFIXES = [
+                "ku",
+                "k",
+                "en",
+                "in",
+                "ci",
+                "c",
+                "un",
+                "a",
+                "i",
+                "an",
+                "e",
+                "eci",
+                "ec",
+            ]
+            prefix_group = f"((?:{'|'.join(PREFIXES)})=)?"
+
+            SUFFIXES = ["an", "as"]
+            suffix_group = f"(=(?:{'|'.join(SUFFIXES)}))?"
+
+            PATTERN = re.compile(
+                rf"{prefix_group}{prefix_group}([a-z/-]+){suffix_group}"
+            )
+
+            parts = PATTERN.match(word)
+
+            if not parts:
+                # print("regex: ", rf"{prefix_group}([a-z]+){suffix_group}")
+                # raise ValueError(f"Cannot process affixes: {word}")
+                return [word]
+
+            return [p for p in parts.groups() if p]
+
+        parts = re.split(r"([-=])", word)
+        if len(parts) == 1:
+            return [word]
+        try:
+            a, sep, b = parts
+        except ValueError:
+            print("-" * 50)
+            print(parts)
+            print("-" * 50)
+            return [word]
+        if len(a) > len(b):
+            return [a, sep + b]
+        else:
+            return [a + sep, b]
+
+    words = [split_affixes(w) for w in words]
+    words = [w for word in words for w in word]
+    return words
