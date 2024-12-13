208e9be5ff7a8c101e301408d543f390df7a787a
mkpoli <git@mkpo.li>
2024-12-13 15:47:52 +0900

    feat: fix tokenizer for edge case such as in word apostrophes

--- utils/src/utils/tokenize.py
+++ utils/src/utils/tokenize.py
@@ -8,10 +8,17 @@ def tokenize(sentence: str) -> list[str]:
     # 下線符号の除去
     sentence = sentence.replace("_", "")
     # 単引用符の除去
-    sentence = re.sub(r"[‘'](.*?)['’]", r"\1", sentence)
-    sentence = re.sub(r"(.*?)['’]", r"\1", sentence)
-    sentence = re.sub(r"[‘'](.*?)", r"\1", sentence)
+    # print("*" * 50)
+    # print(0, sentence)
+    sentence = re.sub(r"(<?!\w)[‘'](.*?)['’]", r"\1", sentence)
+    # print(1, sentence)
+    sentence = re.sub(r"(.*?)['’](?!\w)", r"\1", sentence)
+    # print(2, sentence)
+    sentence = re.sub(r"(<?!\w)[‘'](.*?)", r"\1", sentence)
+    # print(3, sentence)
+    # print("*" * 50)
     sentence = re.sub(r"\[\d+\]", "", sentence)
+    sentence = re.sub(r"\*\d+", "", sentence)
 
     tokenizer = RegexpTokenizer(r"[a-zA-Zâîûêôáíúéó0-9=-_'’]+")
     words = tokenizer.tokenize(sentence)
@@ -57,15 +64,16 @@ def tokenize(sentence: str) -> list[str]:
         try:
             a, sep, b = parts
         except ValueError:
-            print("-" * 50)
-            print(parts)
-            print("-" * 50)
+            # print("-" * 50)
+            # print(parts)
+            # print("-" * 50)
             return [word]
         if len(a) > len(b):
             return [a, sep + b]
         else:
             return [a + sep, b]
 
+    words = [w.strip("‘’'?") for w in words]
     words = [split_affixes(w) for w in words]
     words = [w for word in words for w in word]
     return words
