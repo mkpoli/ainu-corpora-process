a616d15deaa2439d11e7a49fe962a685da243074
mkpoli <git@mkpo.li>
2024-12-30 00:06:21 +0900

    refactor: search use Concordancer

--- utils/src/utils/search.py
+++ utils/src/utils/search.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple
+from typing import Generator, NamedTuple, TypedDict, cast
 
 
 class Keyword(NamedTuple):
@@ -9,6 +9,18 @@ class Keyword(NamedTuple):
 class Word(NamedTuple):
     word: str
     poss: list[str]
+
+
+class Sentence(TypedDict):
+    book: str
+    sentence: str
+    words: list[str]
+    part_of_speech: list[list[str]]
+    translation: str
+
+
+class SearchResult(Sentence):
+    found_intervals: list[tuple[int, int]]
 
 
 def match_word(
@@ -56,3 +68,118 @@ def find(
             return []
 
     return results
+
+
+class Concordancer:
+    corpus: list[Sentence]
+
+    def __init__(self, corpus: list[Sentence]):
+        self.corpus = corpus
+
+    def search(
+        self,
+        keywords: list[Keyword],
+        context_length: int = 1,
+    ) -> Generator[SearchResult, None, None]:
+        for i, sentence in enumerate(self.corpus):
+            previous_contexts = (
+                self.corpus[i - context_length : i] if i - context_length >= 0 else []
+            )
+            next_contexts = (
+                self.corpus[i + 1 : i + context_length + 1]
+                if i + context_length + 1 <= len(self.corpus)
+                else []
+            )
+            previous_contexts = [
+                context
+                for context in previous_contexts
+                if context["book"] == sentence["book"]
+            ]
+            previous_context_words = sum(
+                len(context["words"]) for context in previous_contexts
+            )
+            next_contexts = [
+                context
+                for context in next_contexts
+                if context["book"] == sentence["book"]
+            ]
+            extended_sentence = cast(
+                Sentence,
+                {
+                    "book": sentence["book"],
+                    "sentence": " ".join(
+                        [context["sentence"] for context in previous_contexts]
+                    )
+                    + " "
+                    + sentence["sentence"]
+                    + " "
+                    + " ".join([context["sentence"] for context in next_contexts]),
+                    "words": [
+                        *[
+                            word
+                            for context in previous_contexts
+                            for word in context["words"]
+                        ],
+                        *sentence["words"],
+                        *[
+                            word
+                            for context in next_contexts
+                            for word in context["words"]
+                        ],
+                    ],
+                    "part_of_speech": [
+                        *[
+                            pos
+                            for context in previous_contexts
+                            for pos in context["part_of_speech"]
+                        ],
+                        *sentence["part_of_speech"],
+                        *[
+                            pos
+                            for context in next_contexts
+                            for pos in context["part_of_speech"]
+                        ],
+                    ],
+                    "translation": " ".join(
+                        [context["translation"] for context in previous_contexts]
+                    )
+                    + " "
+                    + sentence["translation"]
+                    + " "
+                    + " ".join([context["translation"] for context in next_contexts]),
+                },
+            )
+
+            extended_words = [
+                Word(word, pos)
+                for word, pos in zip(
+                    extended_sentence["words"], extended_sentence["part_of_speech"]
+                )
+            ]
+
+            previous_contexts_words = sum(
+                len(context["words"]) for context in previous_contexts
+            )
+            next_contexts_words = sum(
+                len(context["words"]) for context in next_contexts
+            )
+            found = find(
+                keywords,
+                extended_words[
+                    previous_contexts_words : len(extended_words) - next_contexts_words
+                ],
+            )
+
+            found_intervals = [
+                (f + previous_context_words, f + len(keywords) + previous_context_words)
+                for f in found
+            ]
+            if found:
+                yield SearchResult(
+                    book=extended_sentence["book"],
+                    sentence=extended_sentence["sentence"],
+                    words=extended_sentence["words"],
+                    part_of_speech=extended_sentence["part_of_speech"],
+                    translation=extended_sentence["translation"],
+                    found_intervals=found_intervals,
+                )
