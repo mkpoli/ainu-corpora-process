0963204c250bf5f621fe10e489a7c8d69853d267
mkpoli <git@mkpo.li>
2025-01-01 01:29:29 +0900

    feat: add CorpusItem to utils.corpus

--- utils/src/utils/corpus.py
+++ utils/src/utils/corpus.py
@@ -5,4 +5,15 @@ class RawSentence(TypedDict):
     sentence: str
     words: list[str]
     translation: str
+    lemmas: list[str]
     part_of_speech: list[list[str]]
+
+
+class CorpusItem(TypedDict):
+    translation: str
+    sentence: str
+    words: list[str]
+    lemmas: list[str]
+    features: list[dict[str, str]]
+    part_of_speech: list[str]
+    glosses: list[list[str]]
