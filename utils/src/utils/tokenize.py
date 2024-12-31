37acb8f50d067db08422eba5a1283343c915185e
mkpoli <git@mkpo.li>
2024-12-31 23:22:37 +0900

    feat: separate lemmatization-like part out from tokenization

--- utils/src/utils/tokenize.py
+++ utils/src/utils/tokenize.py
@@ -1,79 +1,118 @@
 import re
-from nltk.tokenize import RegexpTokenizer
+
+
+def split_affixes(word: str) -> list[str]:
+    if word.count("=") + word.count("-") >= 2:
+        PREFIXES = [
+            "ku",
+            "k",
+            "en",
+            "in",
+            "ci",
+            "c",
+            "un",
+            "a",
+            "i",
+            "an",
+            "e",
+            "eci",
+            "ec",
+        ]
+        prefix_group = f"((?:{'|'.join(PREFIXES)})=)?"
+
+        SUFFIXES = ["an", "as"]
+        suffix_group = f"(=(?:{'|'.join(SUFFIXES)}))?"
+
+        PATTERN = re.compile(rf"{prefix_group}{prefix_group}([a-z/-]+){suffix_group}")
+
+        parts = PATTERN.match(word)
+
+        if not parts:
+            # print("regex: ", rf"{prefix_group}([a-z]+){suffix_group}")
+            # raise ValueError(f"Cannot process affixes: {word}")
+            return [word]
+
+        return [p for p in parts.groups() if p]
+
+    parts = re.split(r"([-=])", word)
+    if len(parts) == 1:
+        return [word]
+    try:
+        a, sep, b = parts
+    except ValueError:
+        # print("-" * 50)
+        # print(parts)
+        # print("-" * 50)
+        return [word]
+    if len(a) > len(b):
+        return [a, sep + b]
+    else:
+        return [a + sep, b]
+
+
+def split_affixing_apostrophes(word: str) -> list[str]:
+    if len(word) < 2:
+        return [word]
+
+    pattern = r"^(['’‘])?([^'’‘]*)(['’‘])?$"
+    match = re.match(pattern, word)
+
+    if not match:
+        return [word]
+
+    prefix, middle, suffix = match.groups()
+
+    if prefix and suffix:
+        return [prefix, middle, suffix]
+    elif prefix:
+        return [prefix, middle]
+    elif suffix:
+        return [middle, suffix]
+    return [word]
 
 
 def tokenize(sentence: str) -> list[str]:
     # 小文字化
-    sentence = sentence.lower()
+    # sentence = sentence.lower()
     # 下線符号の除去
-    sentence = sentence.replace("_", "")
+    # sentence = sentence.replace("_", "")
     # 単引用符の除去
     # print("*" * 50)
     # print(0, sentence)
-    sentence = re.sub(r"(<?!\w)[‘'](.*?)['’]", r"\1", sentence)
+    # sentence = re.sub(r"(<?!\w)[‘'](.*?)['’]", r"\1", sentence)
     # print(1, sentence)
-    sentence = re.sub(r"(.*?)['’](?!\w)", r"\1", sentence)
+    # sentence = re.sub(r"(.*?)['’](?!\w)", r"\1", sentence)
     # print(2, sentence)
-    sentence = re.sub(r"(<?!\w)[‘'](.*?)", r"\1", sentence)
+    # sentence = re.sub(r"(<?!\w)[‘'](.*?)", r"\1", sentence)
     # print(3, sentence)
     # print("*" * 50)
-    sentence = re.sub(r"\[\d+\]", "", sentence)
-    sentence = re.sub(r"\*\d+", "", sentence)
+    # sentence = re.sub(r"\[\d+\]", "", sentence)
+    # sentence = re.sub(r"\*\d+", "", sentence)
 
-    tokenizer = RegexpTokenizer(r"[a-zA-Zâîûêôáíúéó0-9=-_'’]+")
-    words = tokenizer.tokenize(sentence)
+    print("sentence: ", sentence)
+    # tokenizer = RegexpTokenizer(
+    #     r"(?:\.\.\.(\.\.\.)?)|[a-zA-Zâîûêôáíúéó=\-_'’]+|\d+|[<>\.,‘\"“”]"
+    # )
 
-    def split_affixes(word: str) -> list[str]:
-        if word.count("=") + word.count("-") >= 2:
-            PREFIXES = [
-                "ku",
-                "k",
-                "en",
-                "in",
-                "ci",
-                "c",
-                "un",
-                "a",
-                "i",
-                "an",
-                "e",
-                "eci",
-                "ec",
-            ]
-            prefix_group = f"((?:{'|'.join(PREFIXES)})=)?"
+    words = re.split(
+        r"((?:\.\.\.(\.\.\.)?)|[a-zA-Zâîûêôáíúéó=\-_'’]+|\d+|[<>\.,‘\"“”])|\s+",
+        sentence,
+    )
 
-            SUFFIXES = ["an", "as"]
-            suffix_group = f"(=(?:{'|'.join(SUFFIXES)}))?"
+    # words = tokenizer.tokenize(sentence)
 
-            PATTERN = re.compile(
-                rf"{prefix_group}{prefix_group}([a-z/-]+){suffix_group}"
-            )
+    words = [w for w in words if w]
 
-            parts = PATTERN.match(word)
+    print("words (1): ", words)
 
-            if not parts:
-                # print("regex: ", rf"{prefix_group}([a-z]+){suffix_group}")
-                # raise ValueError(f"Cannot process affixes: {word}")
-                return [word]
+    words = [w for word in words for w in split_affixing_apostrophes(word)]
+    print("words (2): ", words)
 
-            return [p for p in parts.groups() if p]
+    words = [w for word in words for w in split_affixes(word)]
+    print("words (3): ", words)
 
-        parts = re.split(r"([-=])", word)
-        if len(parts) == 1:
-            return [word]
-        try:
-            a, sep, b = parts
-        except ValueError:
-            # print("-" * 50)
-            # print(parts)
-            # print("-" * 50)
-            return [word]
-        if len(a) > len(b):
-            return [a, sep + b]
-        else:
-            return [a + sep, b]
+    words = [w for w in words if w]
+    print("words (4): ", words)
 
-    words = [w.strip("‘’'?") for w in words]
-    words = [split_affixes(w) for w in words]
-    words = [w for word in words for w in word]
+    print("-" * 50)
     return words
